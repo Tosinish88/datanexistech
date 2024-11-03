e3bfa6ccb31a38753b96a3a953c44ae4d4ce1581
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-11-03 15:14:54 -0400

    adjusted debug

--- datanexistech/settings.py
+++ datanexistech/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)eq)7b*lrc97%u=r7m$$x11p-5v6c1_&084v1kb*bvg0v%&e=^'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://datanexis-production.up.railway.app', '*']
 
