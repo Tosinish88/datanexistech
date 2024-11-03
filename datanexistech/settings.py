5ef4fe6352cb70030086ddfe113eb679c7c80b65
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-11-03 16:30:56 -0400

    fixed import issue with decouple

--- datanexistech/settings.py
+++ datanexistech/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "lcf8^*c-#g_ly*az!l3s7+c!06xssjnvz!_5ihwg=5w^7!g(ut"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG")
+
 
 ALLOWED_HOSTS = ['https://datanexis-production.up.railway.app', '*']
 
