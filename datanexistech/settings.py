02bb9e873c861d3057fb101b7237de7105e7748b
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-12-07 12:35:24 -0400

    fixed typo error

--- datanexistech/settings.py
+++ datanexistech/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', cast=Csv())
 
