4ab4d8e2090777633c3cc6c5f1084bf0c962b144
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-12-09 08:05:39 -0400

    using a wildcard to test

--- datanexistech/settings.py
+++ datanexistech/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = ["*"]
+
+#config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1,http://localhost',cast=Csv())
 
