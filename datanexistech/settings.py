824b36fe60791301ea6d05564b29ed44a1e31a14
Oluwatosin Ishmeal <olaleyeoluwatosin@gmail.com>
2024-12-17 23:22:42 -0400

    removed irrelevant print

--- datanexistech/settings.py
+++ datanexistech/settings.py
@@ -31,7 +31,6 @@ DEBUG = config('DEBUG')
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
 CSRF_TRUSTED_ORIGINS = config("DJANGO_CSRF_TRUSTED_ORIGINS", default="http://127.0.0.1,http://localhost", cast=Csv())
 
-print(ALLOWED_HOSTS)
 
 # Application definition
 
